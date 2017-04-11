9bcdc55b8bfff0559ecc95bb2b2b2b26bb3fa181
RobinBoons <r.boons@student.fontys.nl>
2017-04-11 13:19:31 +0200

    handball klasse
    implementatie

--- Sport Statistics/Sport Statistics/Handball.cs
+++ Sport Statistics/Sport Statistics/Handball.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace Sport_Statistics
 {
-    class Handball
+    public class Handball : Sport
     {
+        public int TimePenaltys { get; private set; }
+        public int Yellowcards { get; private set; }
+        public int Exclusions { get; private set; }
+        public int Breakouts { get; private set; }
+
+        public Handball(decimal score, decimal attempts, decimal scorePercentage, int timePenaltys, int yellowCards, int exclusions, int breakouts): base(score, attempts, scorePercentage)
+        {
+            if (timePenaltys < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (yellowCards < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (exclusions < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (breakouts < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            TimePenaltys = timePenaltys;
+            Yellowcards = yellowCards;
+            Exclusions = exclusions;
+            Breakouts = breakouts;
+        }
+
+        public void CalculatePercentage()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
