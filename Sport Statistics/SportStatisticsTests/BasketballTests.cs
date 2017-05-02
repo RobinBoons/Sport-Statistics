01a2dbd98322eb6b10cb5c90de26dd6fdc7f08fb
RobinBoons <r.boons@student.fontys.nl>
2017-05-02 13:00:53 +0200

    testklasses
    testklasses aangemaakt voor administration, handball en basketball

---
+++ Sport Statistics/SportStatisticsTests/BasketballTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SportStatisticsTests
+{
+    [TestClass]
+    public class BasketballTests
+    {
+        [TestMethod]
+        public void BasketballConstructorTest()
+        {
+        }
+    }
+}
