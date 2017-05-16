c24aeb80e27e90c2418511452f612a8e7ab9367f
RobinBoons <r.boons@student.fontys.nl>
2017-05-16 12:33:23 +0200

    game
    klasse game toegevoegd om een gamelog bij te houden en te bepalen welk
    team team 1 is en welke team 2

---
+++ Sport Statistics/Sport Statistics/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sport_Statistics
+{
+    public class Game
+    {
+        public Team thuis { get; }
+        public Team uit { get; }
+
+        List<string> GameLog = new List<string>();
+    }
+}
